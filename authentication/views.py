24d5302c403c747b45a22a84f16917c27dd55adf
Fortunat64 <andriamaro6@gmail.com>
2023-04-26 16:11:41 +0300

    Utilisation de vue generique

--- authentication/views.py
+++ authentication/views.py
@@ -1,48 +1,7 @@
-from django.contrib.auth import login, authenticate, logout
-from django.shortcuts import render, redirect
-from django.views.generic import View
-
-from . import forms
-
-class LoginPageView(View):
-    template_name = 'authentication/login.html'
-    form_class = forms.LoginForm
-
-    def get(self,request):
-        form = self.form_class()
-        message = ''
-        return render(request, self.template_name, context={'form': form, 'message': message})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            user = authenticate(
-                username = form.cleaned_data['username'],
-                password = form.cleaned_data['password'],
-            )
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-        message = 'Identifiants invalides.'
-        return render(request, self.template_name, context={'form': form, 'message': message})
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 
 def logout_user(request):
     logout(request)
     return redirect('login')
-    
-def login_page(request):
-    form = forms.LoginForm()
-    message =''
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(
-                username = form.cleaned_data['username'],
-                password = form.cleaned_data['password'],
-            )
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-        message = 'Identifiants invalides.'
-    return render(request, 'authentication/login.html', context={'form': form, 'message': message})
