3939b338e5610b7b0b6da9d2b2a6ac7c79afaed2
Fortunat64 <andriamaro6@gmail.com>
2023-04-27 11:52:20 +0300

    Pages de modification de mot de passe

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 import authentication.views
 import blog.views
 
@@ -24,5 +24,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', authentication.views.login_page, name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
+    path('password_change/', PasswordChangeView.as_view(
+        template_name= 'authentication/password_change_form.html'
+        ), name='password_change'),
+    path(' password_change_done/', PasswordChangeDoneView.as_view(
+        template_name = 'authentication/password_change_done.html'
+    ), name=' password_change_done'),
     path('home/', blog.views.home, name='home'),
 ]
