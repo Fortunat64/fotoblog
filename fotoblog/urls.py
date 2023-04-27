7def1857f0d6ff51f375b36947ed4b92fa34ee6e
Fortunat64 <andriamaro6@gmail.com>
2023-04-27 17:01:45 +0300

    configuration du stockage des médias

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
@@ -33,3 +35,9 @@ urlpatterns = [
     path('signup/', authentication.views.signup_page, name='signup' ),
     path('home/', blog.views.home, name='home'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root = settings.MEDIA_ROOT
+    )
