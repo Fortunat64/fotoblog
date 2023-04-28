97eb65361b377a302a9bab39d1d3c2806c878e29
Fortunat64 <andriamaro6@gmail.com>
2023-04-28 11:12:49 +0300

    mise en place d'un theme

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-from . import forms
+from . import forms, models
 
 @login_required
 def home(request):
-    return render(request, 'blog/home.html')
+    photos = models.Photo.objects.all()
+    return render(request, 'blog/home.html', context={'photos': photos})
 
 
 @login_required
