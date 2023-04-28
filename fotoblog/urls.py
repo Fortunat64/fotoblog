6ea4985bc4d1be32d4ca0f7532c27447fee572ec
Fortunat64 <andriamaro6@gmail.com>
2023-04-28 08:26:56 +0300

    Module upload photos

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     ), name='password_change_done'),
     path('signup/', authentication.views.signup_page, name='signup' ),
     path('home/', blog.views.home, name='home'),
+    path('photo/upload/', blog.views.photo_upload, name='photo_upload')
 ]
 
 
